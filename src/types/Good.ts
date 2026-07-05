10f16d89c39398a6d141baec7b2d8846aace601f
ericagomes-dev <ericagomesdev@gmail.com>
2026-07-05 10:32:54 -0300

    Implement dynamic list of goods

--- src/types/Good.ts
+++ src/types/Good.ts
@@ -1,5 +1,23 @@
-export interface Good {
-  id: number;
-  name: string;
-  color: string;
+/* eslint-disable prettier/prettier */
+import { Good } from '../types/Good';
+
+// eslint-disable-next-line
+const API_URL = 'https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json';
+
+export function getAll(): Promise<Good[]> {
+  return fetch(API_URL).then(response => response.json());
 }
+
+export const get5First = () => {
+  return getAll().then(goods =>
+    [...goods]
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .slice(0, 5),
+  );
+};
+
+export const getRedGoods = () => {
+  return getAll().then(goods =>
+    goods.filter(good => good.color === 'red'),
+  );
+};
